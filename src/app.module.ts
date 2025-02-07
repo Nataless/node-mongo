90bd53f228f30d08750c201f50bfe537ed5e82bb
Natalie Voronova <Nataless@yandex.ru>
2025-02-07 04:55:05 +0300

    saved changes

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    BooksModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_CONNECTION as any),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/books/books.service.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/require-await */
+import { Injectable } from '@nestjs/common';
+import { Book, BookDocument } from '../interfaces/book.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateBookDto } from '../interfaces/dto/create_book.dto';
+
+@Injectable()
+export class BooksService {
+  constructor(@InjectModel(Book.name) private BookModel: Model<BookDocument>) {}
+
+  async create(create_book: CreateBookDto): Promise<BookDocument> {
+    const book = new this.BookModel(create_book);
+    return book.save();
+  }
+
+  async findAll(): Promise<BookDocument[]> {
+    return this.BookModel.find().exec();
+  }
+
+  async getBook(id: string): Promise<BookDocument> {
+    return this.BookModel.findById({ _id: id }).exec() as any;
+  }
+
+  async deleteBook(id: string): Promise<BookDocument> {
+    return this.BookModel.findByIdAndDelete({ _id: id }).exec() as any;
+  }
+
+  async updateBook(id: string, data: CreateBookDto): Promise<BookDocument> {
+    return this.BookModel.findOneAndUpdate({ _id: id }, data).exec() as any;
+  }
+}
